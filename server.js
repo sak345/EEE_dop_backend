53234eae7895c5b3a59bb69e6ed04f10faf93c38
me-t1me <siddhantashoknagar@gmail.com>
2023-07-28 22:53:31 +0530

    dummy changes

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ const PORT = process.env.PORT || 5000;
 
 const server = app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`))
 
+console.log("hello")
 process.on("unhandledRejection", (err, promise) => {
     console.log(`Logged Error: ${err}`);
     server.close(() => process.exit(1));
