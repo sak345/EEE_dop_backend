e2d967b51f23ca330216e9ff79980eeb6d81ec84
Harshit Bhansali <f20200416@hyderabad.bits-pilani.ac.in>
2023-10-20 16:18:05 +0530

    download func added

--- routes/paper.js
+++ routes/paper.js
@@ -2,12 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 const {create, deletePaper, getone, getall, updatepaper} = require("../controllers/Paper");
-const {protect} =  require('../middleware/auth');
+const {protect,roles} =  require('../middleware/auth');
 
 router.route("/create").post(protect, create);
 router.route("/delete").delete(protect, deletePaper);
 router.route("/getone").get(protect, getone);
-router.route("/getall").get(protect, getall);
+router.route("/getall").get(protect,getall);
 router.route("/update").patch(protect, updatepaper);
 
 
